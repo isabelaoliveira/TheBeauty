b36741f26241884ab9ea8b2fb302e86620ef2009
isabelaoliveira <isabella11_@hotmail.com>
2020-08-19 22:52:30 -0300

    Integração Google Calendar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
-                </div>
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23B39DDB&amp;ctz=America%2FSao_Paulo&amp;src=cG5waTJzZm9kcjY2MTF1Y2Q1Ym1wcGxnYWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23A79B8E&amp;title=The%20Beauty&amp;mode=WEEK" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>                </div>
             </div>
         </div>
     </div>
